c56044b1c4d1629ed757ad5fa8c66fdfdf7531e3
charloubels-lang <charloubels@gmail.com>
2025-10-07 16:05:04 +0800

    Add files via upload

---
+++ belosverlasqueuelinkedlist/Student.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package belosverlasqueuelinkedlist;
+
+/**
+ *
+ * @author User
+ */
+  public class Student {
+  
+    private String id;
+    private String name;
+    private String  program;
+    
+    
+    public Student (String id, String name,String program){
+        this.id = id;
+        this.name = name;
+        this.program = program;
+    }
+}
+
+        public String toString() {
+            return "ID: " + "\nName: " +name + "\nProgram: " + program;
+}
+
+      public static void main(String][]args) {
+        
+                Student student = new Student ("S123",Belosverlas","Computer Science""):
+                System.out.println(student1);
+}
+     
+   
+      
